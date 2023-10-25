16da5f52002009b8d8da55b9714bbdc026a46e4c
Flavio Luis Fusi Escalante <fef0031062@est.univalle.edu>
2023-10-25 09:47:01 -0400

    animal DAO

--- NFDao/Model/Animal.cs
+++ NFDao/Model/Animal.cs
@@ -14,13 +14,15 @@ namespace NFDao.Model
         public byte age { get; set; }
         public byte animalCategoryId { get; set; }
         public int userId { get; set; }
+        public byte[] photo { get; set; }
+        public byte shelterId { get; set; }
 
         public Animal()
         {
             
         }
 
-        public Animal(int id, string name, string animalBreed, byte age, byte animalCategoryId, int userId)
+        public Animal(int id, string name, string animalBreed, byte age, byte animalCategoryId, int userId, byte[] photo, byte shelterId)
         {
             this.id = id;
             this.name = name;
@@ -28,15 +30,19 @@ namespace NFDao.Model
             this.age = age;
             this.animalCategoryId = animalCategoryId;
             this.userId = userId;
+            this.photo = photo;
+            this.shelterId = shelterId;
         }
 
-        public Animal(string name, string animalBreed, byte age, byte animalCategoryId, int userId)
+        public Animal(string name, string animalBreed, byte age, byte animalCategoryId, int userId, byte[] photo, byte shelterId)
         {
             this.name = name;
             this.animalBreed = animalBreed;
             this.age = age;
             this.animalCategoryId = animalCategoryId;
             this.userId = userId;
+            this.photo = photo;
+            this.shelterId = shelterId;
         }
     }
 }
