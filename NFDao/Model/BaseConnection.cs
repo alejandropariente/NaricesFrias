718ce1b9a28b94a6277b4e30f1bc42f213b11b2c
alejandropariente <pma0031712@est.univalle.edu>
2023-11-09 11:08:00 -0400

    Cards

--- NFDao/Model/BaseConnection.cs
+++ NFDao/Model/BaseConnection.cs
@@ -13,7 +13,7 @@ namespace NFDao.Model
         private string sever = "DESKTOP-7S64QUV";
         private string dataBase = "DbNaricesFrias";
         private string user = "sa";
-        private string password = ":";
+        private string password = "-";
         private string connectionString;
         private SqlConnection connection;
 
